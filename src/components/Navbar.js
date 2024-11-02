062758bb34f6bee36f0c19e3b9ee521fe02c1164
Carlos Lamego <clamego1@yahoo.com.br>
2024-11-02 20:32:12 -0300

    criada Navbar e adicionada ao App.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+    const logoIcon = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+    return (
+        <nav>
+            <img src={logoIcon}/>
+            <button className="theme-btn"> Mudar o Tema </button>
+            <button className="favorite-btn"> 💖 </button>
+        </nav>
+    )
+}
+
+export default Navbar
